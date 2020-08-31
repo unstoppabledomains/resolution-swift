9b22de7722891f96d01eca85a590df4425d0d223
Sergey Merenkov <sergey@unstoppabledomains.com>
2020-08-31 17:51:05 +0300

    SwiftPM v. 5.3 for support resources inside package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,27 +6,30 @@ import PackageDescription
 let package = Package(
     name: "Resolution",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Resolution",
-            targets: ["Resolution"]),
+            targets: ["Resolution"])
     ],
     dependencies: [
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.3.1"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Resolution",
-            dependencies: ["CryptoSwift"]
-//            resources: [
-//                .process("cnsRegistry.json"),
-//                .process("cnsResolver.json"),
-//            ]
+            dependencies: ["CryptoSwift"],
+            resources: [
+                .copy("ABI/CNS/cnsRegistry.json"),
+                .copy("ABI/CNS/cnsResolver.json")
+            ]
         ),
         .testTarget(
             name: "ResolutionTests",
-            dependencies: ["Resolution"]),
+            dependencies: ["Resolution"],
+            exclude:["Info.plist"],
+            resources: [
+                .copy("ABI/CNS/cnsRegistry.json"),
+                .copy("ABI/CNS/cnsResolver.json")
+            ]
+        )
     ]
 )
