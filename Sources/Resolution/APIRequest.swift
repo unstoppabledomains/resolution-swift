caea36e9554b0f840b12e3088d9e961e91e9d0b1
rommex <roman@unstoppabledomains.com>
2020-09-29 13:43:54 +0300

    Injectable networking layer (#13)
    * introduced protocol NetworkingLayer
    * ReadMe explanation

--- Sources/Resolution/APIRequest.swift
+++ Sources/Resolution/APIRequest.swift
@@ -8,35 +8,60 @@
 
 import Foundation
 
-enum APIError: Error {
+public enum APIError: Error {
     case responseError
     case decodingError
     case encodingError
 }
 
+public protocol NetworkingLayer {
+    func makeHttpPostRequest (url: URL,
+                              httpMethod: String,
+                              httpHeaderContentType: String,
+                              httpBody: JsonRpcPayload,
+                              completion: @escaping(Result<JsonRpcResponse, APIError>) -> Void)
+}
+
 struct APIRequest {
     let url: URL
+    let networking: NetworkingLayer
 
-    init(_ endpoint: String) {
+    init(_ endpoint: String, networking: NetworkingLayer) {
         guard let url = URL(string: endpoint) else {fatalError()}
         self.url = url
+        self.networking = networking
     }
 
     func post(_ body: JsonRpcPayload, completion: @escaping(Result<JsonRpcResponse, APIError>) -> Void ) {
-        do {
-            var urlRequest = URLRequest(url: self.url)
-            urlRequest.httpMethod = "POST"
-            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            urlRequest.httpBody = try JSONEncoder().encode(body)
+        networking.makeHttpPostRequest(url: self.url,
+                                       httpMethod: "POST",
+                                       httpHeaderContentType: "application/json",
+                                       httpBody: body,
+                                       completion: completion)
+    }
+}
 
+public struct DefaultNetworkingLayer: NetworkingLayer {
+    public init() { }
+    
+    public func makeHttpPostRequest(url: URL,
+                             httpMethod: String,
+                             httpHeaderContentType: String,
+                             httpBody: JsonRpcPayload,
+                             completion: @escaping(Result<JsonRpcResponse, APIError>) -> Void) {
+        do {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = httpMethod
+            urlRequest.addValue(httpHeaderContentType, forHTTPHeaderField: "Content-Type")
+            urlRequest.httpBody = try JSONEncoder().encode(httpBody)
             let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, _ in
                 guard let httpResponse = response as? HTTPURLResponse,
-                httpResponse.statusCode == 200,
-                    let jsonData = data else {
-                        completion(.failure(.responseError))
-                        return
+                      httpResponse.statusCode == 200,
+                      let jsonData = data else {
+                    completion(.failure(.responseError))
+                    return
                 }
-
+                
                 do {
                     let result = try JSONDecoder().decode(JsonRpcResponse.self, from: jsonData)
                     completion(.success(result))
@@ -49,5 +74,4 @@ struct APIRequest {
             completion(.failure(.encodingError))
         }
     }
-
 }
